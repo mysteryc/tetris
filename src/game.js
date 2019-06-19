8ca4ce9b17ca534974e6a82363328a378e21d991
mysteryc <dikkens77@gmail.com>
2019-06-19 17:09:50 +0300

    add rotate methods

--- src/game.js
+++ src/game.js
@@ -27,6 +27,9 @@ export default class Game {
     activePiece = {
         x: 0,
         y: 0,
+        get blocks() {
+            return this.rotations[this.rotationIndex];
+        },
         blocks: [
             [0,1,0],
             [1,1,1],
@@ -37,7 +40,7 @@ export default class Game {
     movePieceLeft() {
         this.activePiece.x -= 1;
 
-        if(this.hasCollision()) {
+        if (this.hasCollision()) {
             this.activePiece.x += 1;
         }
     }
@@ -45,7 +48,7 @@ export default class Game {
     movePieceRight() {
         this.activePiece.x += 1;
 
-        if(this.hasCollision()) {
+        if (this.hasCollision()) {
             this.activePiece.x -= 1;
         }
     }
@@ -53,9 +56,42 @@ export default class Game {
     movePieceDown() {
         this.activePiece.y += 1;
 
-        if(this.hasCollision()) {
+        if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+        }
+    }
+
+    rotatePiece() {
+        this.rotateBlock();
+
+        if (this.hasCollision()) {
+            this.rotateBlock(false);
+        }
+    }
+
+    rotateBlock(clockwise = true) {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+        const x = Math.floor(length / 2);
+        const y = length - 1;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = i; j < y - i; j++) {
+                const temp = blocks[i][j];
+
+                if (clockwise) {
+                    blocks[i][j] = blocks[y - j][i];
+                    blocks[y - j][i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[j][y - i];
+                    blocks[j][y - i] = temp;
+                } else {
+                    blocks[i][j] = blocks[j][y - i];
+                    blocks[j][y - i] = blocks[y - i][y - j];
+                    blocks[y - i][y - j] = blocks[y - j][i];
+                    blocks[y - j][i] = temp;
+                }
+            }
         }
     }
 
@@ -64,7 +100,7 @@ export default class Game {
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
-                if(
+                if (
                     blocks[y][x] &&
                     ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
                     this.playfield[pieceY + y][pieceX + x])
@@ -82,7 +118,7 @@ export default class Game {
 
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
-                if(blocks[y][x]) {
+                if (blocks[y][x]) {
                     this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
                 } 
             }
